eab8fcdf50183cefd2b9484132fc3183912cae81
Phoebe Oigara <phoebeoigara@gmail.com>
2023-04-27 09:41:54 +0300

    Practising with react states and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
+const [inCart, setInCart] = useState(false);
+
+const toggleCart = () => {
+setInCart(prevState => !prevState);
+};
+
+const cartClass = inCart ? "in-cart" : "";
+
+return (
+  <li className={cartClass}>
+  <span>{name}</span>
+  <span className="category">{category}</span>
+  <button className="add" onClick={toggleCart}>
+  {inCart ? "Remove From Cart" : "Add to Cart"}
+  </button>
+  </li>
+);
 }
 
 export default Item;
